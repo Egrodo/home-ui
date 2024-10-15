9a0212fb0e3e2577c5cf060dadfbdba3975c2bb0
Noah <egrodo1@gmail.com>
2024-10-14 22:10:55 -0400

    Move global stuff to layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { connectionStore } from '$lib/data/stores';
+	import { handleStateMessage, initWsConnection } from '$lib/data/ws';
+	import { subscribeEntities } from 'home-assistant-js-websocket';
+	import { onMount } from 'svelte';
+
+	// TODO: Is it best to re-connect this on every page mount? Could a better system do this
+	// once per app; do I need any changes in my Svelte configuration to make this a SPA?
+	onMount(async () => {
+		// Initialize connection to websocket. Return callback for unsubscribe on unmount
+		const connection = await initWsConnection();
+		connectionStore.set(connection);
+		const unsubscribe = subscribeEntities(connection, handleStateMessage);
+
+		return unsubscribe;
+	});
+</script>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
+
+	:global(*) {
+		font-family: 'Open Sans', sans-serif;
+		box-sizing: border-box;
+		user-select: none;
+	}
+
+	:global(body) {
+		/* Page size globals */
+		--page-background: #0c0d16;
+		--page-height: 768px;
+		--page-width: 1360px;
+		--page-drawer-background: #1f212e;
+
+		/* Block globals */
+		--block-border-radius: 32px;
+		--block-padding: 38px;
+		--block-default-light-color: #fff7dc;
+		--block-default-dark-color: #1f212e;
+
+		height: var(--page-height);
+		width: var(--page-width);
+		margin: 0;
+	}
+
+	:global(body::-webkit-scrollbar) {
+		display: none;
+	}
+</style>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-	import { connectionStore, selectedLightIdStore } from '$lib/data/stores';
-	import { initWsConnection, handleStateMessage } from '$lib/data/ws';
+	import { selectedLightIdStore } from '$lib/data/stores';
 	import LeftDrawer from '$lib/drawers/left.svelte';
 	import MainDrawer from '$lib/drawers/main.svelte';
 	import RightDrawer from '$lib/drawers/right.svelte';
 	import { Connection, subscribeEntities } from 'home-assistant-js-websocket';
-	import { onMount } from 'svelte';
 
 	// If selectedLight is not null, show the right drawer. Otherwise, hide it.
 	let selectedLightId: string | null = null;
@@ -14,7 +12,6 @@
 	});
 
 	let containerRef: HTMLElement;
-	let connection: Connection;
 
 	// Code to eat a touch event after 5 minutes of inactivity
 	let timerRef: number | undefined;
@@ -31,61 +28,20 @@
 		timerRef = setTimeout(startTimerToEatTouchOnce, 5 * 60 * 1000);
 	};
 
+	/**
+	 * The device goes to sleep after some time, and the user has to touch the
+	 * screen to wake it up. Therefore we should eat that touch event
+	 * so it doesn't accidentally open a menu or something.
+	 *
+	 * 10/10/24: The device doesn't go to sleep rn so no need for this
+	 */
 	function startTimerToEatTouchOnce() {
 		hasEatenTouch = false;
 		containerRef.addEventListener('touchstart', eatTouchAndRestart);
 	}
-
-	onMount(async () => {
-		// Initialize connection to websocket. Return callback for unsubscribe on unmount
-		connection = await initWsConnection();
-		connectionStore.set(connection);
-		const unsubscribe = subscribeEntities(connection, handleStateMessage);
-
-		/**
-		 * The device goes to sleep after some time, and the user has to touch the
-		 * screen to wake it up. Therefore we should eat that touch event
-		 * so it doesn't accidentally open a menu or something.
-		 */
-		startTimerToEatTouchOnce();
-
-		return unsubscribe;
-	});
 </script>
 
 <style>
-	@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
-
-	:global(*) {
-		font-family: 'Open Sans', sans-serif;
-		box-sizing: border-box;
-		user-select: none;
-	}
-
-	:global(body) {
-		/* Page size globals */
-		--page-background: #0c0d16;
-		--page-height: 768px;
-		--page-width: 1360px;
-		--page-drawer-background: #1f212e;
-
-		/* Block globals */
-		--block-border-radius: 32px;
-		--block-padding: 38px;
-		--block-default-light-color: #fff7dc;
-		--block-default-dark-color: #1f212e;
-
-		background-color: var(--page-background);
-
-		height: var(--page-height);
-		width: var(--page-width);
-		margin: 0;
-	}
-
-	:global(body::-webkit-scrollbar) {
-		display: none;
-	}
-
 	.container {
 		background-color: var(--page-background);
 		height: 100%;
